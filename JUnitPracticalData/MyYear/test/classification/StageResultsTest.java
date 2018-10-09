fcf1680730954324c1b04b22910dc9308eefd6a2
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-10-09 12:55:31 +0100

    unit tests written and overcome for testing module grades

--- JUnitPracticalData/MyYear/test/classification/StageResultsTest.java
+++ JUnitPracticalData/MyYear/test/classification/StageResultsTest.java
@@ -82,17 +82,65 @@ public class StageResultsTest {
 
     @Test
     public void testResetValues() {
-        fail("Test not yet implemented"); 
+        //fail("Test not yet implemented"); 
+        System.out.println("Testing resetValues");
+
+        // Set the state of the 'full' object to zeroes
+        full.resetValues();
+
+        // Set expected results
+        int expIntResult = 0;
+        double expDoubleResult = 0.0;
+
+        // Now check each attribute to test that the reset has worked
+        assertEquals("credits", expIntResult, full.getTotalCredits());
+        assertEquals("total", expDoubleResult, full.getTotalMarks(), 0.0);
+
+        // Put the 'full' object back to its original state
+        full.addModuleMark(120, 50.0); 
     }
 
     @Test
     public void testAddModuleMark() {
-        fail("Test not yet implemented"); 
+        //fail("Test not yet implemented"); 
+        
+//        empty.addModuleMark(20, 70.00);
+//        System.out.println(empty.getTotalCredits()); // expected: 20
+//
+//        empty.addModuleMark(10, 70.00); 
+//        System.out.println(empty.getTotalCredits()); // expected: 30
+//
+//        empty.addModuleMark(40, 70.00);
+//        System.out.println(empty.getTotalCredits()); // expected: 70
+        
+        empty.addModuleMark(20, 70.0);
+        assertEquals("credits", 20, empty.getTotalCredits());
+        assertEquals("marks", 140, empty.getTotalMarks(), 0.0);
+        
+        empty.addModuleMark(10, 70.0);
+        assertEquals("credits", 30, empty.getTotalCredits());
+        assertEquals("marks", 210, empty.getTotalMarks(), 0.0);
+        
+        empty.addModuleMark(40, 70.0);
+        assertEquals("credits", 70, empty.getTotalCredits());
+        assertEquals("marks", 490, empty.getTotalMarks(), 0.0);
+        
+        empty.resetValues();
     }
 
     @Test
     public void testCalculateAverageSoFar() {
-        fail("Test not yet implemented"); 
+        //fail("Test not yet implemented"); 
+        // Test with no credits and no marks
+        assertEquals("empty", 0.0, empty.calculateAverageSoFar(), 0.0); 
+        
+        // Test with 120 credits all at 50%
+        assertEquals("full @ 50%", 50.0, full.calculateAverageSoFar(), 0.0); 
+        
+        // Test with 120 credits all at 100%
+        full.resetValues();
+        full.addModuleMark(120, 100.0);
+        assertEquals("full @ 100%", 100.0, full.calculateAverageSoFar(), 0.0); 
     }
 
     @Test
