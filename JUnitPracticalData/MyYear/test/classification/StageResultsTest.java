95c30524f7c324be5cee5eda5f5cc10bda11522f
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-10-09 14:46:00 +0100

    now error checking other full / halfFull objects for the correct string output

--- JUnitPracticalData/MyYear/test/classification/StageResultsTest.java
+++ JUnitPracticalData/MyYear/test/classification/StageResultsTest.java
@@ -15,15 +15,15 @@ import static org.junit.Assert.*;
  * @author James
  */
 public class StageResultsTest {
-    
+
     private StageResults empty; // will have no credits and no marks
     private StageResults full; // will have 120 credits and marks
     private StageResults halfFull; // will have 60 credits and some marks
- 
+
     public StageResultsTest() {
-        
+
     }
-    
+
     @Before
     public void setUp() {
         // empty - object that starts with default values
@@ -37,32 +37,37 @@ public class StageResultsTest {
         // halfFull - object with 60 credits worth of marks and
         // no initial stage2Average
         halfFull = new StageResults();
-        halfFull.addModuleMark(60, 50.0); 
+        halfFull.addModuleMark(60, 50.0);
     }
-    
+
     @After
     public void tearDown() {
-         
+
     }
 
     @Test
     public void testGetStage2Average() {
-         
+
     }
 
     @Test
     public void testGetTotalCredits() {
-         
+
     }
 
     @Test
     public void testGetTotalMarks() {
-        
+
     }
 
     @Test
     public void testSetStage2Average() {
-         
+
+    }
+    
+    @Test
+    public void testFullOperation() {
+        
     }
 
     @Test
@@ -71,13 +76,13 @@ public class StageResultsTest {
         System.out.println("Testing isComplete");
 
         // Check that the empty object is 'not complete'
-        assertFalse("empty object", empty.isComplete()); 
+        assertFalse("empty object", empty.isComplete());
 
         // Check that the full object is 'complete'
-        assertTrue("empty object", full.isComplete()); 
-        
+        assertTrue("empty object", full.isComplete());
+
         // Check that the halfFull object is 'not complete'
-        assertFalse("empty object", halfFull.isComplete()); 
+        assertFalse("empty object", halfFull.isComplete());
     }
 
     @Test
@@ -97,25 +102,25 @@ public class StageResultsTest {
         assertEquals("total", expDoubleResult, full.getTotalMarks(), 0.0);
 
         // Put the 'full' object back to its original state
-        full.addModuleMark(120, 50.0); 
+        full.addModuleMark(120, 50.0);
     }
 
     @Test
     public void testAddModuleMark() {
         //fail("Test not yet implemented"); 
-        
+
         empty.addModuleMark(20, 70.0);
         assertEquals("credits", 20, empty.getTotalCredits());
         assertEquals("marks", 140, empty.getTotalMarks(), 0.0);
-        
+
         empty.addModuleMark(10, 70.0);
         assertEquals("credits", 30, empty.getTotalCredits());
         assertEquals("marks", 210, empty.getTotalMarks(), 0.0);
-        
+
         empty.addModuleMark(40, 70.0);
         assertEquals("credits", 70, empty.getTotalCredits());
         assertEquals("marks", 490, empty.getTotalMarks(), 0.0);
-        
+
         empty.resetValues();
     }
 
@@ -123,38 +128,38 @@ public class StageResultsTest {
     public void testCalculateAverageSoFar() {
         //fail("Test not yet implemented"); 
         // Test with no credits and no marks
-        assertEquals("empty", 0.0, empty.calculateAverageSoFar(), 0.0); 
-        
+        assertEquals("empty", 0.0, empty.calculateAverageSoFar(), 0.0);
+
         // Test with 120 credits all at 50%
-        assertEquals("full @ 50%", 50.0, full.calculateAverageSoFar(), 0.0); 
-        
+        assertEquals("full @ 50%", 50.0, full.calculateAverageSoFar(), 0.0);
+
         // Test with 120 credits all at 100%
         full.resetValues();
         full.addModuleMark(120, 100.0);
         assertEquals("full @ 100%", 100.0, full.calculateAverageSoFar(), 0.0);
-        
+
         // Test with 120 credits all at 42.92%
         full.resetValues();
         full.addModuleMark(120, 43.92);
         assertEquals("full @ 43.92%", 43.92, full.calculateAverageSoFar(), 0.0);
-        
+
         // Restore original values:
         full.resetValues();
         full.addModuleMark(120, 50.0);
-        
+
         // Test half full:
         assertEquals("half full", 50.0, halfFull.calculateAverageSoFar(), 0.0);
-        
+
         halfFull.resetValues();
-        
+
         halfFull.addModuleMark(60, 100);
         assertEquals("half full", 100, halfFull.calculateAverageSoFar(), 0.0);
-        
+
         halfFull.resetValues();
-        
+
         halfFull.addModuleMark(60, 64.77);
         assertEquals("half full", 64.77, halfFull.calculateAverageSoFar(), 0.0);
-        
+
         // Reset original values:
         halfFull.resetValues();
         halfFull.addModuleMark(60, 50.0);
@@ -169,19 +174,29 @@ public class StageResultsTest {
         double[] marks = {0.00, 50.00, 50.00, 100.00, 39.99, 40.0,
             49.99, 50.0, 59.99, 60.0, 69.99, 70.0, 99.99, 35.67,
             44.22, 56.39, 64.00, 76.80};
-        
+
+        double[] stage2 = {0.00, 50.00, 50.00, 100.00, 39.99, 40.00, 49.99,
+            50.00, 59.99, 60.00, 69.99, 70.00, 99.99, 35.67, 44.22, 56.39,
+            64.00, 76.80};
+
         // Array of corresponding classifications with no stage 2 marks
         String[] expResult1 = {"No marks!", "Lower 2nd",
-         "Lower 2nd", "1st", "FAIL", "3rd", "3rd", "Lower 2nd",
-         "Lower 2nd", "Upper 2nd", "Upper 2nd", "1st", "1st",
-         "FAIL", "3rd", "Lower 2nd", "Upper 2nd", "1st"};
+            "Lower 2nd", "1st", "FAIL", "3rd", "3rd", "Lower 2nd",
+            "Lower 2nd", "Upper 2nd", "Upper 2nd", "1st", "1st",
+            "FAIL", "3rd", "Lower 2nd", "Upper 2nd", "1st"};
+
+        String[] expResult2 = {"No marks!", "Lower 2nd", "Lower 2nd",
+            "1st", "FAIL", "3rd", "3rd", "Lower 2nd", "Lower 2nd", "Upper 2nd", 
+            "Upper 2nd", "1st", "1st", "FAIL", "3rd", "Lower 2nd", "Upper 2nd", "1st"};
 
         // Run tests with no stage 2 average
         for (int count = 0; count < marks.length; count++) {
-         full.resetValues();
-         full.addModuleMark(120, marks[count]);
-         assertEquals("120 credits, mark = " + marks[count], expResult1[count],
-         full.predictClass());
+            full.resetValues();
+            full.addModuleMark(120, marks[count]);
+            assertEquals("120 credits, mark = " + marks[count], expResult1[count], full.predictClass());
         }
-    }  
+        
+        assertEquals("not enough credits", "Insufficient credits", empty.predictClass());
+        assertEquals("not enough credits", "Insufficient credits", halfFull.predictClass());
+    }
 }
