ec4699c1b6142a1e7fc7aba88fa1ab6ccd1ca133
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-10-14 19:25:42 +0100

    test class created for bank account class

---
+++ BankingApplicationPracticalDemo/test/bankentities/BankAccountTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankentities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author James
+ */
+public class BankAccountTest {
+    
+    
+    
+    public BankAccountTest() {
+        
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+        
+    }
+
+    @Test
+    public void testDepositFunds() {
+        
+    }
+
+    @Test
+    public void testTakeFunds() {
+        
+    }
+
+    @Test
+    public void testGetBalance() {
+        
+    }
+
+    @Test
+    public void testGetOverdraft() {
+        
+    }
+
+    @Test
+    public void testGetAccountHolder() {
+        
+    }
+    
+}
