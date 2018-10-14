bf8487e1f3746f6ec4369f4fbdb291a7f66ea633
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-10-14 19:55:01 +0100

    all test classes written and all tests passed

--- BankingApplicationPracticalDemo/test/bankentities/BankAccountTest.java
+++ BankingApplicationPracticalDemo/test/bankentities/BankAccountTest.java
@@ -16,10 +16,13 @@ import static org.junit.Assert.*;
  */
 public class BankAccountTest {
     
-    
+    BankAccount testAccount;
     
     public BankAccountTest() {
-        
+        testAccount = new BankAccount("James Hunt");
+        // Holder = James Hunt
+        // Balance = 100
+        // Overdraft = 500
     }
     
     @Before
@@ -34,27 +37,35 @@ public class BankAccountTest {
 
     @Test
     public void testDepositFunds() {
-        
+        testAccount.depositFunds(100); 
+        assertEquals(200, testAccount.getBalance());
+        testAccount.takeFunds(100);
     }
 
     @Test
     public void testTakeFunds() {
+        // Test with an amount that can be taken:
+        testAccount.takeFunds(50);
+        assertEquals(50, testAccount.getBalance());
+        testAccount.depositFunds(50);
         
+        // Test with an amount that is too much to be taken:
+        assertFalse(testAccount.takeFunds(200));
     }
 
     @Test
     public void testGetBalance() {
-        
+        assertEquals(100, testAccount.getBalance()); 
     }
 
     @Test
     public void testGetOverdraft() {
-        
+        assertEquals(500, testAccount.getOverdraft());  
     }
 
     @Test
     public void testGetAccountHolder() {
-        
+        assertEquals("James Hunt", testAccount.getAccountHolder());
     }
     
 }
