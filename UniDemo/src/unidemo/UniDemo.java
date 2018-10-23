e911e34394b18964d78a3dbb5f9341dde9f13313
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-10-23 11:12:52 +0100

    demo university system initial files created

---
+++ UniDemo/src/unidemo/UniDemo.java
@@ -0,0 +1,16 @@
+package unidemo;
+
+/**
+ *
+ * @author James
+ */
+public class UniDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
